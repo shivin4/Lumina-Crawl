93bb399bd236cb26239a1b1c25ea9045ad3d97b7
shivin khandelwal <shivin2004@gmail.com>
2025-12-07 11:48:00 +0530

    feat: bootstrap WebsiteQualityAnalyzerApplication entrypoint

---
+++ src/main/java/com/qualityanalyzer/WebsiteQualityAnalyzerApplication.java
@@ -0,0 +1,12 @@
+package com.qualityanalyzer;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class WebsiteQualityAnalyzerApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(WebsiteQualityAnalyzerApplication.class, args);
+    }
+}
